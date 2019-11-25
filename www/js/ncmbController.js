8e342e1a96b63d9593a65325c7e738b7e7f92edd
k18rs003 <k18rs003@st.kyusan-u.ac.jp>
2019-11-25 13:56:25 +0900

    ncmbControllerの初期化

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -2,4 +2,17 @@
 
 
 var ncmbController = {
+  APPLICATION_KEY:appKey,
+  CLIENR_KEY: clientKey,
+
+  ncmb: null,
+    currentUser: null,  // ログインしたユーザーのオブジェクトを格納
+    screenSize: null,    // 画面サイズを格納
+
+    // 初期化
+    init: function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+    }
 }
